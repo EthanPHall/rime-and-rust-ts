32a60f1a523f85209db98960843bcf5f5123116c
Ethan Hall <ephallbusiness@gmail.com>
2024-04-07 18:44:44 -0400

    Crafting Section for the Caravan Screen is mocked up

--- src/components/caravan/CaravanSectionCrafting/CaravanSectionCrafting.tsx
+++ src/components/caravan/CaravanSectionCrafting/CaravanSectionCrafting.tsx
@@ -4,8 +4,53 @@ import './CaravanSectionCrafting.css';
 interface CaravanSectionCraftingProps {}
 
 const CaravanSectionCrafting: FC<CaravanSectionCraftingProps> = () => (
-  <div className="CaravanSectionCrafting" data-testid="CaravanSectionCrafting">
-    CaravanSectionCrafting Component
+  <div className="caravan-section-crafting" data-testid="caravan-section-crafting">
+    <div className='grid-parent'>
+      <div className='crafting-section'>
+        <div className='sled-crafting-section'>
+          <div className='sled-name first'>Sled Name</div>
+          <div className='sled-crafting-recipes'>
+            <button>Item 1</button>
+            <button>Item 2</button>
+          </div>
+          <div className='sled-crafting-recipes'>
+            <button>Item 3</button>
+            <button>Item 4</button>
+          </div>
+        </div>
+        <div className='sled-crafting-section'>
+          <div className='sled-name'>Sled Name</div>
+          <div className='sled-crafting-recipes'>
+            <button>Item 1</button>
+            <button>Item 2</button>
+            {/* <button>Item 3</button>
+            <button>Item 4</button> */}
+          </div>
+        </div>
+        <div className='sled-crafting-section'>
+          <div className='sled-name first'>Sled Name</div>
+          <div className='sled-crafting-recipes'>
+            <button>Item 1</button>
+            <button>Item 2</button>
+          </div>
+          <div className='sled-crafting-recipes'>
+            <button>Item 3</button>
+            <button>Item 4</button>
+          </div>
+        </div>
+      </div>
+      <div className='trade-section'>
+        <div className='trade-section-title'>
+          Trade
+        </div>
+        <button>Item 1</button>
+        <button>Item 2</button>
+        <button>Item 3</button>
+        <button>Item 4</button>
+        <button>Item 5</button>
+        <button>Item 6</button>
+      </div>
+    </div>
   </div>
 );
 
