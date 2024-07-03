f3c0b2264c8806293d2d197d7a92acef6f5c583d
Ethan Hall <ephallbusiness@gmail.com>
2024-07-03 00:40:24 -0400

    Should be able to add sleds to the sled list now via crafting, but still need to actually allow sleds to craft other sleds. Like a sledsThisCanCraft field in sleds.json, something like that.

--- src/components/caravan/CaravanParent/CaravanParent.tsx
+++ src/components/caravan/CaravanParent/CaravanParent.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useContext, useEffect, useState } from 'react';
 import './CaravanParent.css';
-import CaravanSectionCrafting, { Sled } from '../CaravanSectionCrafting/CaravanSectionCrafting';
+import CaravanSectionCrafting, { Sled, SledSeed } from '../CaravanSectionCrafting/CaravanSectionCrafting';
 import CaravanSectionNavBar from '../CaravanSectionNavBar/CaravanSectionNavBar';
 import CaravanSectionValuables, { ResourceFactory, ResourcePlusQuantity, ResourcePlusQuantityList, ResourcePlusQuantityUtil, ResourceUtils, ResourcesList } from '../CaravanSectionValuables/CaravanSectionValuables';
 import CaravanSectionOptions from '../CaravanSectionOptions/CaravanSectionOptions';
@@ -42,13 +42,33 @@ const CaravanParent: FC<CaravanParentProps> = () => {
     progressionContext.setFlags(newFlags);
   }, [resources]);
 
-  const [sleds, setSleds] = useState<Sled[]>([
-    Sled.create("Scavenger Sled"),
-    Sled.create("Scavenger Sled"),
-    Sled.create("Forge Sled"),
-    Sled.create("Scavenger Sled"),
-    Sled.create("Scavenger Sled"),
-  ]);
+  //Define a type that contains a sled and then the
+  //quantity of said sled. Use a Map to represent the Sled list as a whole
+  type SledMapType = Map<string, SledPlusQuantity>;
+  type SledPlusQuantity = {
+    "sled":Sled,
+    "quantity":number
+  }
+  type SledMapEntry =[    
+    string,
+    SledPlusQuantity
+  ]
+  function createSledMapEntry(sled:Sled, quantity:number):SledMapEntry{
+    return [sled.name, {"sled": sled, "quantity": quantity}];
+  }
+  const [sleds, setSleds] = useState<SledMapType>(
+    new Map().set(...createSledMapEntry(Sled.create("Scavenger Sled"), 1))
+      .set(...createSledMapEntry(Sled.create("Forge Sled"), 1))
+  );
+  //To make the new sleds list play well with other components, make a function that translates the Map to an Array
+  function sledsMapToArray():Sled[]{
+    const sledsArray:Sled[] = [];
+    sleds.forEach((value) => {
+      sledsArray.push(value.sled);
+    })
+
+    return sledsArray;
+  }
 
   //Load the tradable list with all resources that are tradeable
   const resourcesList:ResourcesList = resourceData;
@@ -63,6 +83,15 @@ const CaravanParent: FC<CaravanParentProps> = () => {
     tradableListDefault
   );
 
+  function isResourceNamePlusQuantityArray(
+    arg:ResourceNamePlusQuantity[]|SledPlusQuantity[]
+  ): arg is ResourceNamePlusQuantity[]{
+    //This function doesn't hand;e when the array is of leangth 0, and the couple attempts I've made to change that
+    //have failed. TODO: Try to make this handle 0 length arrays, but it's not a priority.
+
+    return "resource" in arg[0];
+  }
+  
   function modifyResources(resourceMods:ResourceNamePlusQuantity[]): boolean{
     let result:boolean = true;
 
@@ -99,18 +128,89 @@ const CaravanParent: FC<CaravanParentProps> = () => {
 
     return result;
   }
-
-  function exchangeResources(toConsume:ResourceNamePlusQuantity[], toGenerate:ResourceNamePlusQuantity[]){
-    const resourceModsNegative:ResourceNamePlusQuantity[] = toConsume.map((resourceMod) => {
-      if(resourceMod.quantity > 0){
-        return {resource: resourceMod.resource, quantity: -resourceMod.quantity};
+  function modifySleds(sledMods:SledPlusQuantity[]):boolean{
+    //Copy sleds list
+    const newSledsMap:SledMapType = new Map(sleds);
+    
+    //modify the new list
+    sledMods.forEach((mod) => {
+      const entry:SledPlusQuantity|undefined = newSledsMap.get(mod.sled.name);
+      if(entry){
+        entry.quantity += 1;
+        newSledsMap.set(mod.sled.name, entry);
       }
-
-      return resourceMod;
+      else{
+        newSledsMap.set(mod.sled.name, {
+          "sled": mod.sled,
+          "quantity": mod.quantity
+        })
+      }
+    });
+    
+    //find if any of the qunatities in the list are negative
+    let someQuantityWasNegative:boolean = false;
+    newSledsMap.forEach((value) => {
+      someQuantityWasNegative = value.quantity < 0
+      if(!someQuantityWasNegative){
+        return;
+      }
     });
 
-    if(modifyResources(resourceModsNegative)){
-      modifyResources(toGenerate);
+    //If there are negative values, don't modify the sled list state, and return false
+    if(someQuantityWasNegative){
+      return false;
+    }
+    else{
+      //Else, return true and modify list
+      setSleds(newSledsMap);
+      return true;
+    }
+
+  }
+
+  function modifyItemQuantities(modifications:ResourceNamePlusQuantity[]|SledPlusQuantity[]): boolean{
+    if(modifications.length > 0){
+      if(isResourceNamePlusQuantityArray(modifications)){
+        modifyResources(modifications);
+      }
+      else{
+        modifySleds(modifications);
+      }
+    }
+
+    return true;
+  }
+
+  function exchangeResources(toConsume:ResourceNamePlusQuantity[]|SledPlusQuantity[], toGenerate:ResourceNamePlusQuantity[]|SledPlusQuantity[]){
+    //If teh consume list is empty, jump to generating
+    if(toConsume.length == 0){
+      modifyItemQuantities(toGenerate);
+    }
+    else if(isResourceNamePlusQuantityArray(toConsume)){
+      const resourceModsNegative:ResourceNamePlusQuantity[] = toConsume.map((resourceMod) => {
+        if(resourceMod.quantity > 0){
+          return {resource: resourceMod.resource, quantity: -resourceMod.quantity};
+        }
+  
+        return resourceMod;
+      });
+  
+      if(modifyItemQuantities(resourceModsNegative)){
+        modifyItemQuantities(toGenerate);
+      }
+    }
+    else{
+      const sledModsNegative:SledPlusQuantity[] = toConsume.map((sledMod) => {
+        if(sledMod.quantity > 0){
+          return {sled: sledMod.sled, quantity: -sledMod.quantity};
+        }
+  
+        return sledMod;
+      });
+  
+      if(modifyItemQuantities(sledModsNegative)){
+        modifyItemQuantities(toGenerate);
+      }
     }
   }
 
@@ -121,7 +221,7 @@ const CaravanParent: FC<CaravanParentProps> = () => {
         <div className='nav-rendered-composite'>
           <CaravanSectionNavBar getSectionBeingDisplayed={getSectionToDisplay} setSectionToDisplay={setSectionToDisplay}></CaravanSectionNavBar>
           
-          {sectionToDisplay==CaravanSectionNames.CRAFTING && <CaravanSectionCrafting sleds={sleds} tradeResources={tradableList} exchangeResources={exchangeResources}></CaravanSectionCrafting>}
+          {sectionToDisplay==CaravanSectionNames.CRAFTING && <CaravanSectionCrafting sleds={sledsMapToArray()} tradeResources={tradableList} exchangeResources={exchangeResources}></CaravanSectionCrafting>}
           {sectionToDisplay==CaravanSectionNames.SLEDS && <CaravanSectionSleds></CaravanSectionSleds>}
           {sectionToDisplay==CaravanSectionNames.EXPLORATION && <CaravanSectionExploration></CaravanSectionExploration>}
         </div>
