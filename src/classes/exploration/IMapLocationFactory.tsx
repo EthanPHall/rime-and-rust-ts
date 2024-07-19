e145ef144bf842a5deb2b561415d94c1abeea029
Ethan Hall <ephallbusiness@gmail.com>
2024-07-18 23:23:04 -0400

    Found a noise library that seems to work fine, though I do still need to test the exploration map

--- src/classes/exploration/IMapLocationFactory.tsx
+++ src/classes/exploration/IMapLocationFactory.tsx
@@ -13,6 +13,12 @@ interface IMapLocationFactory {
      * @param data This method creates a location with the exact data provided. This is useful for creating a location with a specific key.
      */
     createExactLocation(data:MapLocationData):IMapLocation;
+
+    /**
+     * 
+     * @param data This method chooses which background location to generate, so key and name may be ingored. Otherwise, uses the rest of the data to fill the new Location's data. Make sure that position is the location's position within the entire map, not within a chunk or other smaller area.
+     */
+    createBackgroundLocation(data:MapLocationData):IMapLocation;
 }
 
 export default IMapLocationFactory;
--- src/classes/exploration/MapLocationFactoryJSONPerlinNoise.tsx
+++ src/classes/exploration/MapLocationFactoryJSONPerlinNoise.tsx
@@ -1,14 +1,17 @@
+import Noise from "noise-ts";
 import IMapLocation from "./IMapLocation";
 import IMapLocationFactory from "./IMapLocationFactory";
 import MapLocationData from "./MapLocationData";
+import explorationLocationData from "../../data/exploration/exploration-location-data.json";
+import MapLocationJSON from "./MapLocationJSON";
 
 class MapLocationFactoryJSONPerlinNoise implements IMapLocationFactory {
     private seed: number;
-    private perlinNoise: any;
+    private noise: Noise;
 
     constructor(seed: number){
         this.seed = seed;
-        this.perlinNoise = new Noise(seed);
+        this.noise = new Noise(seed);
     }
 
     createLocationWithData(data: MapLocationData): IMapLocation {
@@ -16,6 +19,34 @@ class MapLocationFactoryJSONPerlinNoise implements IMapLocationFactory {
     }
     createExactLocation(data: MapLocationData): IMapLocation {
         throw new Error("Method not implemented.");
+    }
+    createBackgroundLocation(data: MapLocationData): IMapLocation {
+        //Get the noise value for the location's position. Multiply is by 100 to fit with the ranges defined in the exploration-location-data.json file.
+        const noiseValue = this.noise.perlin2(data.getPosition().x, data.getPosition().y) * 100;
+
+        //Get a list of the background locations
+        const backgroundLocations = explorationLocationData.locations.filter(location => explorationLocationData.backgroundLocations.includes(location.key));
+
+        //Find the background location that fits the noise value
+        const locationData = backgroundLocations.find(location => location.range.min <= noiseValue && location.range.max >= noiseValue);
+
+        //Is location data defined?
+        if(locationData){
+            //Yes: Create the location
+            return new MapLocationJSON(data.getPosition(), locationData.name, locationData.key, data.getCleared(), data.getRevealed(), data.getFloating());
+        }
+        else{
+            //No: Is there a default location?
+            if(backgroundLocations[0]){
+                //Yes: Create the location
+                console.log(`No background location data found for noise value ${noiseValue}. Using default location.`);
+                return new MapLocationJSON(data.getPosition(), backgroundLocations[0].name, backgroundLocations[0].key, data.getCleared(), data.getRevealed(), data.getFloating());
+            }
+            else{
+                //No: Throw an error
+                throw new Error(`No background location data found for noise value ${noiseValue}. No default location found.`);
+            }
+        }
     }
 }
 
