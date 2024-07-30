328fda69e293da9ba05396812460db0390c6a59d
Ethan Hall <ephallbusiness@gmail.com>
2024-07-30 00:02:57 -0400

    There was a bug with the array scrambler where it wasn't properly filtering out indices that had been used. Was leading to enemies spawning and overlapping eachother. That should be fixed now.

--- src/classes/utility/ArrayScrambler.tsx
+++ src/classes/utility/ArrayScrambler.tsx
@@ -1,16 +1,18 @@
 class ArrayScrambler{
     static scrambleArray<T>(array:T[]) : T[]{
         const scrambled = [];
-        const validIndices:number[] = [];
+        let validIndices:number[] = [];
+
         for(let i = 0; i < array.length; i++){
             validIndices.push(i);
         }
+
+        
         for(let i = 0; i < array.length; i++){
-            const chosenIndex = Math.floor(Math.random() * validIndices.length);
-            scrambled.push(array[chosenIndex]);
-            validIndices.filter((index) => {
-                return index != chosenIndex;
-            })
+            const indexRaw = Math.random() * validIndices.length;
+            const chosenIndex = Math.floor(indexRaw);
+            scrambled.push(array[validIndices[chosenIndex]]); 
+            validIndices.splice(chosenIndex, 1);
         }
         
         return scrambled;
